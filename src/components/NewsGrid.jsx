1ab5b019b65e6f2ddedba914b22559ed4c534996
Azfar-Tariq <73424313+Azfar-Tariq@users.noreply.github.com>
2024-08-27 12:24:27 +0500

    Update NewsGrid.jsx

--- src/components/NewsGrid.jsx
+++ src/components/NewsGrid.jsx
@@ -1,14 +1,14 @@
-import NewsItem from './NewsItem'
+import React from 'react';
+import NewsItem from './NewsItem';
 
-function NewsGrid({items}){
-    return (
-        <div className="news-grid">
-        {items.map((item, i) => (
-            <NewsItem key={i} item={item} />
-            ))}
-
-        </div>
-    )
+function NewsGrid({ items }) {
+  return (
+    <div className="news-grid">
+      {items.map((item, i) => (
+        <NewsItem key={`${item.url}-${i}`} item={item} />
+      ))}
+    </div>
+  );
 }
 
-export default NewsGrid
+export default NewsGrid;
